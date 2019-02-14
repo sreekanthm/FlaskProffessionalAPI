bebf03f41993a0e37710992ca185c47a4cbaaefc
Sreekanth <sreekanth.kanth@gmail.com>
2019-02-14 12:30:58 +0530

    Flask API first Commit

---
+++ application.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_mongoengine import MongoEngine
+
+db = MongoEngine()
+
+def create_app(**config_overrides):
+    app = Flask(__name__)
+
+    #Load config
+    app.config.from_pyfile('settings.py')
+
+    #apply overrides for Test
+    app.config.update(config_overrides)
+
+    # setup db
+    db.init_app(app)
+
+    #import blueprints
+    from home.views import home_app
+    from pet.views import pet_app
+
+    # register blueprints
+    app.register_blueprint(home_app)
+    app.register_blueprint(pet_app)
+
+    return app
